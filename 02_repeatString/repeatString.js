99f9228fd5224e927dfd247f98a0a087c9b7a29a
RagingD0nkey <152647125+RagingD0nkey@users.noreply.github.com>
2024-03-08 17:46:54 +0100

    Second exercise done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+
+const repeatString = function(text, times) {
+
+    if (times<0) return "ERROR";
+
+    let result="";
+
+    while (times>0) {
+        result+=text;
+        times--;
+    }
+    return result;
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
