e19e6bfdb6e98d78763065dee8f2301d399a185c
RagingD0nkey <152647125+RagingD0nkey@users.noreply.github.com>
2024-03-11 22:58:45 +0100

    Sixth exercise done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(!Number.isInteger(year)) return "ERROR";
+
+    // if (year%4==0){
+    //     if ((year%100==0)&&(year%400==0)){
+    //         return true;
+    //     }
+    //     else if ((year%100==0)&&(year%400!=0)){
+    //         return false;
+    //     }
+    //     else if (year%100!=0) {
+    //         return true;
+    //     }
+    // }
+    // else {
+    //     return false;
+    // }
+
+    
 
 };
 
