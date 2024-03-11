09a9052bc8260a3820151cd7b625d40bbfad7d62
RagingD0nkey <152647125+RagingD0nkey@users.noreply.github.com>
2024-03-11 20:05:25 +0100

    Fifth exercise done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
 
+    if (!Number.isInteger(a)||!Number.isInteger(b)) return "ERROR"; //non number parameters
+    if ((a<0)||(b<0)) return "ERROR"; //positive numbers
+
+    if (a > b) {
+        const temp = a;
+        a = b;
+        b = temp;
+      }
+ 
+    let sum =0;
+
+    for (let i =a;i<=b;i++) {
+        sum+=i;
+    }
+
+    return sum;
 };
+
+console.log(sumAll(-40,10));
 
 // Do not edit below this line
 module.exports = sumAll;
