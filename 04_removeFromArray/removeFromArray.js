3ff9112272cd13975c2e466ad7f05740c0fd31c2
RagingD0nkey <152647125+RagingD0nkey@users.noreply.github.com>
2024-03-11 19:33:43 +0100

    Fourth exercise done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(initial,...params) {
+    const paramsArr = Array.from(params);
+    const initialArr = Array.from(initial);
+    let finalArr=[];
 
+    outer: for (i=0;i<initialArr.length;i++) {
+        for (j=0;j<paramsArr.length;j++) {
+            if (initialArr[i]===paramsArr[j]) {
+                continue outer;
+            }
+        }
+
+        finalArr.push(initialArr[i]);
+      }
+
+      return finalArr;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
